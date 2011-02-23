0f732e63cb9796a9bc60f39c95cd8e0e4344d5f8
Mountaniol <shizosoft@gmail.com>
2011-02-23 19:08:26 +0200

    A working core of cbs already done: 2 levels hash and objects inserting looks well

--- cbs.h
+++ cbs.h
@@ -3,7 +3,7 @@
 
 #include "obj.h"
 #include "l.h"
-#include <pthread.h>
+#include "lock.h"
 
 /*  CBS (Central Bus Station) is a mechanism which dedicated to several goals: 
     1. It links an object ID to the  object; 
@@ -20,8 +20,8 @@
 #define CBS_HASH_1  1024
 #define CBS_HASH_2  128
 
-#define CBS_COUNT_1(id)         (id /   CBS_HASH_2 )
-#define CBS_COUNT_2(id)         (id % CBS_HASH_2 )
+#define CBSH1(id)         (id /   CBS_HASH_2 )
+#define CBSH2(id)         (id % CBS_HASH_2 )
 
 /* This macro detect if given ID belongs to a hash or not */
 #define THIS_HASH(id, num)      (id < (num * CBS_HASH_1 * CBS_HASH_2) )
@@ -30,25 +30,44 @@
 struct cbs_o_struct
 {
     obj_t t;
-    obj_t * o;                          /* The object */
-    obj_t * manager;            /* Manager of this object */
+    obj_t * o;              /* The object it holds  */
+    obj_t * manager;        /* Manager of this object */
     que_t * q_employee;     /* Linked list of employees of this object  */
 };
 
 typedef struct cbs_o_struct cbs_o;
 
-struct cbs_hash_struct
+struct cbs_hash2_struct
 {
-    obj_t t;
-    cbs_o * o[1024];
-    struct cbs_hash_struct * next;
-    long amount;    /* Amount: used to free unused nodes. */
-    que_t * free_id;
-    id_t max_id;            /* Maximal id; never decreases */
+    obj_t 						t;					/* Object abstraction */
+    struct cbs_hash_struct * 	next;				/* Pointer to the next */
+    long 						amount;    			/* Amount: used to free unused nodes. */
+	olock_t 					lock;
+	obj_t 						*o[CBS_HASH_2];				/* Next level hash / Objects  */
 };
 
-typedef struct cbs_hash_struct cbs_hash_t; 
+typedef struct cbs_hash2_struct cbs_hash2_t; 
 
+
+
+struct cbs_hash1_struct
+{
+    obj_t 						t;					/* Object abstraction */
+    struct cbs_hash_struct * 	next;				/* Pointer to the next */
+    long 						amount;    			/* Amount: used to free unused nodes. */
+	olock_t 					lock;
+	cbs_hash2_t					*o[CBS_HASH_1];				/* Next level hash / Objects  */
+};
+
+typedef struct cbs_hash1_struct cbs_hash1_t; 
+
+
+
+
+int cbs_init(void);
+int cbs_destroy();
+id_t cbs_get_id();
+int cbs_return_id(obj_t * o);
 
 
 
