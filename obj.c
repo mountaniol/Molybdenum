e96fdac440748df8899c1b8f649fd655951a2a39
Mountaniol <shizosoft@gmail.com>
2011-02-24 18:10:23 +0200

    obj_init now runs here; added obj_err()

--- obj.c
+++ obj.c
@@ -11,6 +11,7 @@
 #include "dw.h"
 #include "l.h"
 #include "lock.h"
+#include "cbs.h"
 
 int	st_obj_free(void);
 
@@ -56,8 +57,19 @@ obj_e obj_init(obj_t * ps_o, type_e t)
 	/* If the object not registred - return error */
 	if (!obj_f_array[t]) return( OBJ_E_TYPE );
 
-	ps_o->type = t;
 	olock_init(&ps_o->lock);
+	olock_lock(&ps_o->lock);
+	ps_o->type = t;
+	ps_o->id = cbs_get_id();
+	olock_unlock(&ps_o->lock);
+	if (!ps_o->id) 
+	{
+		ps_o->error = OBJ_E_ID;
+		return(OBJ_E_ID);
+	}
+
+	/* If all right then insert the object int CBS */
+	cbs_insert_obj(ps_o);
 	return(OBJ_E_OK);
 }
 
@@ -65,9 +77,23 @@ obj_e obj_init(obj_t * ps_o, type_e t)
 obj_e obj_finish(obj_t * ps_o)
 {
 	/* If the object not registred - return error */
+	cbs_remove_obj(ps_o);
+	olock_lock(&ps_o->lock);
+	cbs_return_id(ps_o);
 	ps_o->type = OBJ_TYPE_NONE;
+	olock_unlock(&ps_o->lock);
 	olock_destroy(&ps_o->lock);
 	return(OBJ_E_OK);
+}
+
+
+obj_e obj_err(obj_t * o)
+{
+	obj_e e;
+	if (!o) return(OBJ_E_MEMORY);
+	e = o->error;
+	o->error = OBJ_E_OK;
+	return(e);
 }
 
 
@@ -80,14 +106,26 @@ obj_t * obj_dup(obj_t * ps_o)
 
 obj_t * obj_new(type_e t, void * ps_v)
 {
-	if (obj_f_array[t] ) return(obj_f_array[t]->new(ps_v));
-	return(NULL);
+	obj_t * o = NULL;
+	if (obj_f_array[t] )
+	{
+		o = obj_f_array[t]->new(ps_v);
+		obj_init(o,t);
+	}
+	return(o);
 }
 
 
 int obj_free(obj_t * ps_o)
 {
-	if (ps_o && obj_f_array[ps_o->type]) return(obj_f_array[ps_o->type]->free(ps_o));
+	type_e e;
+	if (ps_o && obj_f_array[ps_o->type]) 
+	{
+		e = ps_o->type;
+		obj_finish(ps_o);
+		return(obj_f_array[e]->free(ps_o));
+	}
+
 	return(1);
 }
 
@@ -130,4 +168,6 @@ obj_t * obj_same(obj_t * ps_a, obj_t * ps_b)
 	if (ps_a && ps_b && obj_f_array[ps_a->type]) return(obj_f_array[ps_a->type]->same(ps_a, ps_b));
 	return(NULL);
 }
+
+
 
