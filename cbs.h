4410576746642acea3203defdd696ef21fd82847
Mountaniol <shizosoft@gmail.com>
2011-02-24 18:01:43 +0200

    Modifyed cbs_o, cbs_hash2_t, cbs_hash1_t ; added several defines for working woth hash ; added function prototypes

--- cbs.h
+++ cbs.h
@@ -29,21 +29,20 @@
 /* This structure holds an object */
 struct cbs_o_struct
 {
-    obj_t t;
-    obj_t * o;              /* The object it holds  */
-    obj_t * manager;        /* Manager of this object */
-    que_t * q_employee;     /* Linked list of employees of this object  */
+    obj_t 					t;
+    obj_t * 				o;              /* The object it holds  */
+    struct cbs_o_struct * 	manager;        /* Manager of this object */
+    que_t * 				q_employee;     /* Linked list of employees of this object  */
 };
 
 typedef struct cbs_o_struct cbs_o;
 
 struct cbs_hash2_struct
 {
-    obj_t 						t;					/* Object abstraction */
-    struct cbs_hash_struct * 	next;				/* Pointer to the next */
-    long 						amount;    			/* Amount: used to free unused nodes. */
-	olock_t 					lock;
-	obj_t 						*o[CBS_HASH_2];				/* Next level hash / Objects  */
+    obj_t 						t;							/* Object abstraction */
+    struct cbs_hash_struct * 	next;						/* Pointer to the next */
+    long 						amount;    					/* Amount: used to free unused nodes. */
+	cbs_o 						*o[CBS_HASH_2];				/* Next level hash / Objects  */
 };
 
 typedef struct cbs_hash2_struct cbs_hash2_t; 
@@ -55,7 +54,6 @@ struct cbs_hash1_struct
     obj_t 						t;					/* Object abstraction */
     struct cbs_hash_struct * 	next;				/* Pointer to the next */
     long 						amount;    			/* Amount: used to free unused nodes. */
-	olock_t 					lock;
 	cbs_hash2_t					*o[CBS_HASH_1];				/* Next level hash / Objects  */
 };
 
@@ -63,12 +61,25 @@ typedef struct cbs_hash1_struct cbs_hash1_t;
 
 
 
+#define CBS_WRONG_ID(o) (o->id < 1)
+#define CBS_WRING_TYPE(o) ((o->type == OBJ_TYPE_CBS_HASH1) || (o->type == OBJ_TYPE_CBS_HASH2) || (o->type == OBJ_TYPE_CBS_O))
 
-int cbs_init(void);
-int cbs_destroy();
-id_t cbs_get_id();
-int cbs_return_id(obj_t * o);
+#define CBS_OBJ_WRONG(o)	(!o | CBS_WRONG_ID(o) | CBS_WRING_TYPE(o))
 
+
+#define CBS_OBJ_TO_CBS(j) (cbs_hash->o[ CBSH1(j->id) ]->o[CBSH2(j->id)])
+
+
+int 	cbs_init(void);
+int 	cbs_destroy();
+id_t 	cbs_get_id();
+int 	cbs_return_id(obj_t * o);
+
+int 	cbs_insert_obj(obj_t * o);
+int 	cbs_remove_obj(obj_t * o);
+
+long 	cbs_hash_amount();
+int 	cbs_set_employee(obj_t * m, obj_t * e);
 
 
 #endif /*  _cbs_s_dkfjhgdkj32jjhkjh9k7j8h9kjhkj */
