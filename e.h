f27c726b86cb2a0391777efcb1b82e1da54ccd96
Mountaniol <shizosoft@gmail.com>
2011-02-24 18:06:56 +0200

    Added OBJ_E_INDEX, OBJ_E_ID ; added obj_err_to_str prototype

--- e.h
+++ e.h
@@ -13,8 +13,14 @@ typedef enum object_error
 	OBJ_E_TYPE = 0xFFFF,	/* Asked wrong operation for type */
 	OBJ_E_UNKNOWN, 			/* Returned on obj_init if the object type not registred */
 	OBJ_E_ARG,				/* Wrong argument */
-	OBJ_E_MEMORY			/* Memory error: can't allocate for example */
+	OBJ_E_MEMORY,			/* Memory error: can't allocate for example */
+	OBJ_E_INDEX,			/* Asked wrong index */
+	OBJ_E_ID				/* Wrong ID of object. Reason: uninitialised CBS */
 } obj_e ;
+
+
+struct obj_struct;
+int obj_err_to_str(struct obj_struct * o, char * pc_str, int i_strlen);
 
 #endif /* _e_h_ashdfasdftasd542312763583975340968dkjfghd */
 
